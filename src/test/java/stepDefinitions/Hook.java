1342939c0eed834d301e0202eb013693e49533be
Samir Shah <samirshh9@gmail.com>
2022-06-03 20:34:32 +0530

    Integrated reporting

--- src/test/java/stepDefinitions/Hook.java
+++ src/test/java/stepDefinitions/Hook.java
@@ -1,7 +1,16 @@
 package stepDefinitions;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Scenario;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import utils.TestContextSetup;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Hook {
     public TestContextSetup contextSetup;
@@ -13,5 +22,16 @@ public class Hook {
     @After
     public void closeBrowser() {
         contextSetup.driverFactory.getWebDriver().quit();
+    }
+
+    @AfterStep
+    public void addScreenShot(Scenario scenario) throws IOException {
+       WebDriver driver = contextSetup.driverFactory.getWebDriver();
+       if(scenario.isFailed()){
+           File sourcePath=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+           byte[] fileContent = FileUtils.readFileToByteArray(sourcePath);
+           scenario.attach(fileContent,"image/png","image");
+       }
+
     }
 }
